f885486cd34d26d17d150711bfd30ac2e57808c5
PGitHubCosta <41348058+PGitHubCosta@users.noreply.github.com>
2026-06-09 19:28:49 +0000

    Atualizado

--- PerfilAluno/Controllers/HomeController.cs
+++ PerfilAluno/Controllers/HomeController.cs
@@ -17,6 +17,12 @@ public class HomeController : Controller
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Sobre()
+    {
+    
+    return View();
+    }
+
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- PerfilAluno/obj/Debug/net10.0/PerfilAluno.AssemblyInfo.cs
+++ PerfilAluno/obj/Debug/net10.0/PerfilAluno.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PerfilAluno")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b7a093b795e13aaf0339b570b588c8fb4c15ed6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5323b679795dcd1fafa6a59b0e911ee4b57e47e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PerfilAluno")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PerfilAluno")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
